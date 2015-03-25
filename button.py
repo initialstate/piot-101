25e1f3a0e0c8feece63f5b61cedc3e46d809be4e
David Sulpy <david@sulpy.com>
2015-03-25 17:37:59 -0500

    fixed some naming conventions and fixed a line ending issue

--- button.py
+++ button.py
@@ -6,13 +6,14 @@ io.setwarnings(False)
 # Numbering scheme that corresponds to breakout board and pin layout 
 io.setmode(io.BCM)
 
-pinNumBTN = 23
-io.setup(pinNumBTN, io.IN) # Specify that pinNumBTN will be an input prevInput = 0
+button_pin = 23
+io.setup(button_pin, io.IN) # Specify that button_pin will be an input
+prev_input = 0
 
 while True:
     # Get the state of the button input
-    btnInput = io.input(pinNumBTN)
-    if (btnInput != prevInput):
+    button_input = io.input(button_pin)
+    if (button_input != prev_input):
         # When the button changes state, print its value
-        print btnInput
-        prevInput = btnInput
+        print button_input
+        prev_input = button_input
