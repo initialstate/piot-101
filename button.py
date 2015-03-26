70a2479df0d5b459e4792203d18ddd9ced861291
David Sulpy <david@sulpy.com>
2015-03-25 19:36:31 -0500

    fixed a line ending issue in button.py

--- button.py
+++ button.py
@@ -7,12 +7,13 @@ io.setwarnings(False)
 io.setmode(io.BCM)
 
 button_io_pin = 23
-io.setup(button_io_pin, io.IN) # Specify that button_io_pin will be an input prevInput = 0
+io.setup(button_io_pin, io.IN) # Specify that button_io_pin will be an input
+button_previous_input = 0
 
 while True:
     # Get the state of the button input
     button_input = io.input(button_io_pin)
-    if (button_input != prevInput):
+    if (button_input != button_previous_input):
         # When the button changes state, print its value
         print button_input
-        prevInput = button_input
+        button_previous_input = button_input
