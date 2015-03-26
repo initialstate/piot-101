a0158007adc90ba3cb25aab69bd34f69a83b5043
David Sulpy <david@sulpy.com>
2015-03-26 00:48:23 +0000

    fixed variable names

--- button-led.py
+++ button-led.py
@@ -11,7 +11,7 @@ io.setmode(io.BCM)
 led_io_pin = 4
 button_io_pin = 23
 # Specifies that led_pin will be an output
-io.setup(led_pin, io.OUT)
+io.setup(led_io_pin, io.OUT)
 # Specifies that button_io_pin will be an input
 io.setup(button_io_pin, io.IN)
 
@@ -22,15 +22,15 @@ while True:
     # Get the state of the button input
     button_input = io.input(button_io_pin)
     # Debounce the button
-    if ((not prev_input) and button_input):
+    if ((not previous_button_input) and button_input):
         # Toggle the button on and off
         button_on = not button_on
-    prev_input = button_input
+    previous_button_input = button_input
     sleep(0.05)
     
     if button_on:
         # Turn the LED on
-        io.output(led_pin, io.HIGH)
+        io.output(led_io_pin, io.HIGH)
     else:
         # Turn the LED off
-        io.output(led_pin, io.LOW)
+        io.output(led_io_pin, io.LOW)
